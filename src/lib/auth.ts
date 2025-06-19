0f9d04a9109909914cb30404d5d157891ac42150
arya <aryagsv@gmail.com>
2025-06-19 14:14:25 -0400

    Updated auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,7 +38,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
